2d1490835ea8ae24cc4ff689f176f13ab4307512
Thomas GENTILHOMME <gentilhomme.thomas@gmail.com>
2019-03-28 01:29:27 +0100

    chore: complete d.ts

---
+++ index.d.ts
@@ -0,0 +1,20 @@
+declare namespace jsdoc {
+    interface linkBlock {
+        _orphans: block[];
+        [name: string]: block;
+    }
+
+    interface block {
+        [key: string]: string;
+        members?: block[];
+    }
+
+    interface response {
+        [file: string]: linkBlock;
+    }
+}
+
+declare function jsdoc(dir: string): Promise<jsdoc.response>;
+
+export as namespace jsdoc;
+export = jsdoc;
